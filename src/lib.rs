83e5c44e9493e2fa0430684ae505d97f3a3371ab
Gerardo Moran Cruz <gerardo.moran@ndeveloper.com>
2021-10-06 15:55:39 -0500

    compilacion inicial sin errores

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,9 @@
-use solana_program::{
-    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, msg, pubkey::Pubkey,
-};
+pub mod entrypoint;
+pub mod instructions;
+pub mod error;
 
-entrypoint!(process_instruction);
-fn process_instruction(
-    program_id: &Pubkey,
-    accounts: &[AccountInfo],
-    instruction_data: &[u8],
-) -> ProgramResult {
-    msg!(
-        "process_instruction: {}: {} accounts, data={:?}",
-        program_id,
-        accounts.len(),
-        instruction_data
-    );
-    Ok(())
-}
 
+/*
 #[cfg(test)]
 mod test {
     use {
@@ -52,3 +39,4 @@ mod test {
         assert_matches!(banks_client.process_transaction(transaction).await, Ok(()));
     }
 }
+*/
